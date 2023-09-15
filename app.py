190fac667b57169baa3398af7ac636475bd38279
Ettienne Smit <devel@assemblage24.com>
2023-09-15 12:58:26 +0200

    Working scraper

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ from pages.quotes_page import QuotesPage
 page_content = requests.get('https://quotes.toscrape.com').content
 page = QuotesPage(page_content)
 
-print(page.quotes)
+for quote in page.quotes:
+    print(quote) # __repr__ replies
