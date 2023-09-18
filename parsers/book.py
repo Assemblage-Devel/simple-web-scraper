5b90119c7ef674e8d842f6ef36496dd873acac59
Ettienne Smit <devel@assemblage24.com>
2023-09-18 21:00:02 +0200

    add link and rating

--- parsers/book.py
+++ parsers/book.py
@@ -2,6 +2,8 @@ import re
 from locators.book_locators import BookLocators
 
 
+domain = 'https://books.toscrape.com/'
+
 class BookParser:
     """
     Given a quote div, find the elements (content, author, tags)
@@ -11,12 +13,17 @@ class BookParser:
         self.parent = parent
 
     def __repr__(self): # 'represent' from course: (64)
-        return f'Book: {self.title}, Price: {self.price}'
+        return f'Book: {self.title}, link: {self.link}, rating: {self.rating} Price: {self.price}'
 
     @property
     def title(self):
         locator = BookLocators.TITLE
-        return self.parent.select_one(locator)['title']
+        return self.parent.select_one(locator)['title'] # or .attrs['title']
+     
+    @property
+    def link(self):
+        locator = BookLocators.LINK
+        return f"{domain}{self.parent.select_one(locator)['href']}" # or .attrs['title']
     
     @property
     def price(self):
@@ -31,4 +38,8 @@ class BookParser:
     @property
     def rating(self):
         locator = BookLocators.RATING
-        return ''
+        value = self.parent.select_one(locator)['class'] # [star-rating, Three]
+        print(value)
+        # list comprehention is better
+        rating = [p for p in value if p != 'star-rating']
+        return rating[0]
