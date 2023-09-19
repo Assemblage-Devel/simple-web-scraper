c1fc8d2ec46c24305bff2dad8d10d297f5c562d3
Ettienne Smit <devel@assemblage24.com>
2023-09-20 00:55:59 +0200

    extend menu options

--- menu.py
+++ menu.py
@@ -2,13 +2,21 @@ from app import books, quotes
 
 
 USER_CHOICE = """
-- 'b' to look at 5-star books
+- 'a' show all books
+- 'b' show top 10 5-star books
 - 'c' to look at cheapest books
 - 'n' next available book on the page
+- 't' total books
 - 'v' view inspirational quotes
 - 'q' quit
 
 Enter your choice: """
+
+
+def print_all_books():
+    for book in books:
+        print(book)
+
 
 def print_best_books():
     best_books = sorted(books, key=lambda x: (x.rating * -1, x.price))[:10] # first sort by rating, then by price for similar items
@@ -17,15 +25,20 @@ def print_best_books():
 
 
 def print_cheapest_books():
-    cheapest_books = sorted(books, key=lambda x: x.price)
+    cheapest_books = sorted(books, key=lambda x: x.price)[:10]
     for book in cheapest_books:
         print(book)
 
 
 books_generator = (e for e in books)
 
+
 def get_next_book():
     print(next(books_generator))
+
+
+def total_books():
+    print(len(books))
 
 
 def view_quotes():
@@ -34,16 +47,18 @@ def view_quotes():
 
 
 user_choices = {
+    'a': print_all_books,
     'b': print_best_books,
     'c': print_cheapest_books,
     'n': get_next_book,
+    't': total_books,
     'v': view_quotes
 }
 
 def menu():
     choice = input(USER_CHOICE)
     while choice != 'q':
-        if choice in ('b','c','n','v'):
+        if choice in ('a','b','c','n','t','v'):
             user_choices[choice]()
         else:
             print('That is not a valid selection, try again.')
