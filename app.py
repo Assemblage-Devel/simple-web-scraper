b04cd3260c97a5d21b89d441bbde724073e93038
Ettienne Smit <devel@assemblage24.com>
2023-09-20 00:58:12 +0200

    add code to check all pages

--- app.py
+++ app.py
@@ -8,11 +8,16 @@ quote_page_content = requests.get('https://quotes.toscrape.com').content
 quote_page = QuotesPage(quote_page_content)
 quotes = quote_page.quotes
 
-
-book_page_content = requests.get('https://books.toscrape.com/').content
+book_page_content = requests.get('https://books.toscrape.com/catalogue/page-1.html').content
 book_page = BooksPage(book_page_content)
+
 books = book_page.books
 
+for page in range(1, book_page.page_count):
+    url = f'https://books.toscrape.com/catalogue/page-{page+1}.html'
+    book_page_content = requests.get(url).content
+    book_page = BooksPage(book_page_content)
+    books.extend(book_page.books)
 
 
 
